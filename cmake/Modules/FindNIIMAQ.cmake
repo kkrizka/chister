a3fdd7b35b3a30843c460917b86c5c82a74053a9
Karol Krizka <kkrizka@gmail.com>
2017-06-20 07:55:18 -0700

    Whitespace

--- cmake/Modules/FindNIIMAQ.cmake
+++ cmake/Modules/FindNIIMAQ.cmake
@@ -5,13 +5,12 @@
 #   NIIMAQ_LIBRARIES    - libraries you need to link to
 
 FIND_PATH(NIIMAQ_INCLUDE_DIR niimaq.h
-			     PATHS
-				"${NIIMAQ_DIR}/Include"
-       			     DOC "Diretory containing niimaq.h include file")
+  PATHS "${NIIMAQ_DIR}/Include"
+  DOC "Diretory containing niimaq.h include file")
 
 FIND_LIBRARY(NIIMAQ_LIBRARIES NAMES imaq
-			      PATHS "${NIIMAQ_DIR}/Lib/MSVC"
-			      DOC "niimaq library for linking")
+  PATHS "${NIIMAQ_DIR}/Lib/MSVC"
+  DOC "niimaq library for linking")
 
 INCLUDE(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(NIIMAQ DEFAULT_MSG NIIMAQ_INCLUDE_DIR NIIMAQ_LIBRARIES)
